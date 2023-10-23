4768467c9d44b670da6614545d73ad3f05fcb2db
Q-Afandi <muhammadafandihidayat2003@gmail.com>
2023-10-23 12:48:53 +0700

    Upload tugas MVC

---
+++ app/models/User_model.php
@@ -0,0 +1,12 @@
+<?php 
+
+class User_model {
+    private $nama = 'Mohamad Afandi Hidayat';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
+
+?>
---
+++ app/views/home/index.php
@@ -0,0 +1,10 @@
+
+<div class="container">
+    <div class="jumbotron mt-4">
+        <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+        <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
+        <hr class="my-4">
+        <p>Ini adalah tampilan utama dari hasil video MVC web programming unpas</p>
+        <a class="btn btn-primary btn-lg" href="https://www.youtube.com/playlist?list=PLFIM0718LjIVEh_d-h5wAjsdv2W4SAtkx" role="button">Learn more</a>
+    </div>
+</div>
